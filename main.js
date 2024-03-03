62a85c1d3651134919e1bd06dfdb646f6577d56a
Kristian Ziu <k.ziu@binnova.it>
2024-03-04 00:36:23 +0100

    Triple cube update

--- main.js
+++ main.js
@@ -1,40 +1,64 @@
 import "./style.css";
-
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 
+//Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.setZ(0);
+camera.position.setX(100);
+camera.position.setY(0);
 
-const renderer = new THREE.WebGLRenderer({
+//Light
+const light = new THREE.AmbientLight(0xffffff, 3);
+light.position.set(1, 1, 1).normalize();
+scene.add(light);
+
+//Geometries
+const cube = new THREE.Mesh(new THREE.BoxGeometry(15, 15, 15), new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff }));
+//const cube2 = new THREE.Mesh(new THREE.BoxGeometry(20, 20, 20), new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff }));
+//const cube3 = new THREE.Mesh(new THREE.BoxGeometry(25, 25, 25), new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff }));
+
+cube.position.set(0, 0, 0);
+// cube2.position.set(25, 35, 0);
+// cube3.position.set(12, 90, 0);
+
+camera.lookAt(cube.position);
+
+scene.add(cube);
+// scene.add(cube2);
+// scene.add(cube3);
+
+//Renderer
+var renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector("#bg"),
+    antialias: true,
 });
-
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const torus = new THREE.Mesh(geometry, material);
-
-scene.add(torus);
-
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(10, 10, 50);
-
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
+//Camera Controls
+var controls = new OrbitControls(camera, renderer.domElement);
+controls.autoRotate = true;
+// controls.autoRotateSpeed = [10];
 
 function animate() {
+    controls.update();
     requestAnimationFrame(animate);
-
-    torus.rotation.x += 0.01;
-    torus.rotation.y += 0.005;
-    torus.rotation.z += 0.01;
-
     renderer.render(scene, camera);
-}
 
-renderer.render(scene, camera);
+    // cube.rotation.y += 0.005;
+    // cube.rotation.z += 0.005;
+
+    // cube2.rotation.x += 0.003;
+    // cube2.rotation.z += 0.003;
+
+    // cube3.rotation.x += 0.001;
+    // cube3.rotation.z += 0.001;
+    // cube3.rotation.z += 0.001;
+
+    // controls.update();
+}
 animate();
